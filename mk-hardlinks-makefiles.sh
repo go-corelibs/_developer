a4b1ba0cff1308d0a326a984fb4ab30b70b1c1c2
Kevin C. Krinke <kevin@krinke.ca>
2024-01-20 12:11:12 -0500

    mk-hardlinks-makefiles.sh: rewritten for better code re-use; restored golang alias; added debian alias; implemented CMD=echo nop support

--- mk-hardlinks-makefiles.sh
+++ mk-hardlinks-makefiles.sh
@@ -3,22 +3,37 @@ SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CORELIBS_PATH="$(dirname "${SCRIPT_PATH}")"
 MAKEFILES_PATH="${SCRIPT_PATH}/common-makefiles"
 
+#CMD=echo
+
 link_makefile () {
     sel=$(basename "${1}")
     src="${MAKEFILES_PATH}/${sel}"
-    if [ -f "${src}" ]
+    if [ ! -f "${src}" ]
     then
-        rm -f "${sel}"
-        ln -v "${src}" "${sel}"
-        return 0
+        sel="Golang.${sel}.mk"
+        src="${MAKEFILES_PATH}/${sel}"
+        [ ! -f "${src}" ] && return 1
     fi
-    return 1
+    ${CMD} rm -f "${sel}"
+    ${CMD} ln -v "${src}" "${sel}"
+    return 0
+}
+
+link_makefiles () {
+    while [ $# -gt 0 ]
+    do
+        for f in $(ls -1 ${1} | sort -V)
+        do
+            link_makefile "${f}"
+        done
+        shift
+    done
 }
 
 if [ $# -eq 0 ]
 then
     echo "usage: $(basename $0) <name> [name...]"
-    echo "       $(basename $0) all"
+    echo "       $(basename $0) <all|debian|golang>"
     echo
     echo "names:"
     for name in $(ls -1 ${MAKEFILES_PATH} | sort -V)
@@ -30,26 +45,21 @@ fi
 
 case "${1}" in
     "all"|"All"|"ALL")
-        for f in $(ls -1 ${MAKEFILES_PATH} | sort -V)
-        do
-            link_makefile "${f}"
-        done
+        link_makefiles ${MAKEFILES_PATH}
+        exit
+        ;;
+    "debian"|"Debian")
+        link_makefiles ${MAKEFILES_PATH}/Debian.*mk
+        exit
+        ;;
+    "golang"|"Golang")
+        link_makefiles ${MAKEFILES_PATH}/Golang.*mk
         exit
         ;;
 esac
 
 for mf in "${@}"
 do
-    #: check exact name first
     link_makefile "${mf}"
-    if [ $? -ne 0 ]
-    then
-        #: check Golang.{name}.mk next
-        link_makefile "Golang.${mf}.mk"
-        if [ $? -ne 0 ]
-        then
-            #: not a thing
-            echo "# not found: ${mf}"
-        fi
-    fi
+    [ $? -ne 0 ] && echo "# not found: ${mf}"
 done
