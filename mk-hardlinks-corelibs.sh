a1a328d14dd48041ef2ca4edeb3d963209657233
Kevin C. Krinke <kevin@krinke.ca>
2024-02-08 14:02:57 -0500

    mk-hardlinks-corelibs.sh: correct MAKEFILE_PATH setting

--- mk-hardlinks-corelibs.sh
+++ mk-hardlinks-corelibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CORELIBS_PATH="$(dirname "${SCRIPT_PATH}")"
-MAKEFILE_PATH="${SCRIPT_PATH}/template/CoreLibs.mk"
+MAKEFILE_PATH="${SCRIPT_PATH}/template-corelibs-pkg/CoreLibs.mk"
 
 pushd "${CORELIBS_PATH}" > /dev/null
 for pkg in $(ls -1 -d * | grep -v "_developer")
