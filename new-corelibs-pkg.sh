d5df4397b67eafb55831b4bfbae9f979d929e397
Kevin C. Krinke <kevin@krinke.ca>
2024-02-08 14:04:16 -0500

    new-corelibs-pkg.sh: check for coreutils rpl binary; support new test file; add some commentary verbosity

--- new-corelibs-pkg.sh
+++ new-corelibs-pkg.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CORELIBS_PATH="$(dirname "${SCRIPT_PATH}")"
-RPL_BIN=$(which rpl.linux.arm64)
+RPL_BIN=$(which rpl)
+if [ -z "${RPL_BIN}" ]
+then
+    echo "error: rpl command not found"
+    echo "       go install github.com/go-curses/coreutils-replace/cmd/rpl@latest"
+    exit 1
+fi
 
 if [ $# -ne 3 ]
 then
@@ -47,26 +53,42 @@ echo "# creating: ${DST}"
 cp -Rv "${SCRIPT_PATH}/template-corelibs-pkg" "${DST}"
 pushd "${DST}" > /dev/null
 
+echo "# renaming: .go files"
 mv -v tmplpkgname.go ${NAME}.go
-${RPL_BIN} -ra 'tmplpkgdir'         "${DIRNAME}"
-${RPL_BIN} -ra 'tmplpkgname'        "${NAME}"
-${RPL_BIN} -ra 'TMPLPKGNAME'        "${KEY}"
-${RPL_BIN} -ra 'tmplpkgsummary'     "${SUMMARY}"
-${RPL_BIN} -ra 'tmplpkgdescription' "${DESCRIPTION}"
+mv -v tmplpkgname_test.go ${NAME}_test.go
+
+echo "# updating: all files"
+${RPL_BIN} -Ra 'tmplpkgdir'         "${DIRNAME}"
+${RPL_BIN} -Ra 'tmplpkgname'        "${NAME}"
+${RPL_BIN} -Ra 'TMPLPKGNAME'        "${KEY}"
+${RPL_BIN} -Ra 'tmplpkgsummary'     "${SUMMARY}"
+${RPL_BIN} -Ra 'tmplpkgdescription' "${DESCRIPTION}"
+
+echo "# go.mod: init, tidy"
 go mod init
 go mod tidy
+
+echo "# git: init"
 git init .
+
+echo "# git: add .gitignore"
 git add .gitignore
 git commit -m ".gitignore: added git ignorance"
 for f in LICENSE AUTHORS.md
 do
+    echo "# git: add ${f}"
     git add "${f}"
     git commit -m "${f}: added $(basename "${f}" ".md") file"
 done
+
 CL_VER=$(grep CORELIBS_MK_VERSION CoreLibs.mk | awk '{print $3}')
+echo "# git: add CoreLibs.mk (${CL_VER})"
 git add CoreLibs.mk
 git commit -m "CoreLibs.mk: added ${CL_VER}"
+
+echo "# git: add Makefile (v0.0.0)"
 git add Makefile
 git commit -m "Makefile: added go-corelibs Makefile"
+
 popd > /dev/null
 echo "# done"
